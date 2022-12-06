da9ed064484dbdd16ff13db7c3485a7146d6ca8b
faisalnawaz <42169344+faisalnawaz@users.noreply.github.com>
2022-12-07 00:49:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express()
 const port = 3100;
 
-app.get('/', (req, res) => res.send('Testing App 27'));
+app.get('/', (req, res) => res.send('Testing App 28'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
